915d02704aa6d44a75e09711bfaf754b60d7fef7
Vishnu Tej Nanda Kumar <Vishnu.Nanda.Kumar@smu.ca>
2023-04-07 15:52:54 -0300

    django assignment for MCDA5550 Mobile Web and CLoud App Development

---
+++ a00465519_django_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Hotels(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    price = models.IntegerField()
+    available_room_count = models.IntegerField(null=True)
